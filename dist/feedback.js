e51d9dda0f02b38c4a4f2f6b17204279f53429a3
Chen <chenop@gmail.com>
2016-10-19 00:02:30 +0300

    - Added dist directory

---
+++ dist/feedback.js
@@ -0,0 +1,41 @@
+/*!
+ * angular-directive-boilerplate
+ * 
+ * Version: 0.0.8 - 2016-10-12T22:33:59.080Z
+ * License: MIT
+ */
+
+
+'use strict';
+
+angular.module('feedback.module', ['ngAnimate'])
+	.directive('feedback', function () {
+		return {
+			scope: {
+				callback: '='
+			},
+			restrict: 'AE',
+			templateUrl: 'feedback.html',
+			replace: true,
+			controller: function($scope) {
+				$scope.toggleTabVisibility = function() {
+					// TODO use angular-animation to do the toggle
+					$("#feedback-form").toggle("slide");
+				};
+
+				$scope.submit = function() {
+					$scope.data = {
+						email: $scope.feedbackEmail,
+						content: $scope.feedbackContent
+					};
+					if ($scope.callback)
+						$scope.callback($scope.data);
+				};
+
+			},
+			link: function ($scope) {
+			}
+		};
+	});
+
+angular.module("feedback.module").run(["$templateCache", function($templateCache) {$templateCache.put("feedback.html","<div id=\"feedback\"><div id=\"feedback-form\" ng-submit=\"submit()\" class=\"animate-show\"><form class=\"form panel-body\" role=\"form\"><div class=\"form-group\"><input class=\"form-control\" ng-model=\"feedbackEmail\" name=\"email\" autofocus=\"\" placeholder=\"Your e-mail\" type=\"email\"></div><div class=\"form-group\"><textarea class=\"form-control\" name=\"body\" ng-model=\"feedbackContent\" required=\"\" placeholder=\"Please write your feedback here...\" rows=\"5\">\n            </textarea></div><button class=\"btn btn-primary pull-right\">Send</button></form></div><div id=\"feedback-tab\" ng-click=\"toggleTabVisibility()\">Feedback</div></div>");}]);
